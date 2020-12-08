9028397ac415c7bdfacbda4883ddb04600ef2484
will <walnuts@gmail.com>
2020-12-07 20:25:54 -0800

    add page width and pc screen resolution

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,6 +1,5 @@
 //Get the button
 var toTop = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() { scrollFunction() };
 
@@ -11,9 +10,14 @@ function scrollFunction() {
         toTop.style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+// A function to show screen dimentions and page width
+function getScreenDimentions() {
+    document.documentElement.scrollHeight
+    document.getElementById("dimenstions").innerHTML = "Page Width: " + window.innerWidth + "<br/>" + "Display Width : " + screen.width + " Pixels" + "<br/> Display Height : " + screen.height + " Pixels";
 }
